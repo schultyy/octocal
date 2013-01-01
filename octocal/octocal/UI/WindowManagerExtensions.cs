284c81068ad390f04f1ec9cc2312b26a37261d8a
Jan Schulte <janschulte90@googlemail.com>
2013-01-01 18:27:31 +0100

    new childwindow backgroundcolor.

--- octocal/octocal/UI/WindowManagerExtensions.cs
+++ octocal/octocal/UI/WindowManagerExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Dynamic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using Caliburn.Micro;
 using Castle.Windsor;
@@ -19,15 +20,25 @@ namespace octocal.UI
         {
             dynamic options = new ExpandoObject();
 
+            var parentWindow = Application.Current.MainWindow;
+
+            var width = ((ContentControl)parentWindow.Content).ActualWidth;
+            var height = ((ContentControl)parentWindow.Content).ActualHeight;
+
             options.WindowStyle = WindowStyle.None;
             options.ShowInTaskbar = false;
             options.AllowsTransparency = true;
-            options.Background = new SolidColorBrush(Colors.Black);
+            options.Background = new SolidColorBrush(Color.FromRgb(51, 51, 51));
             options.Foreground = new SolidColorBrush(Colors.White);
-            options.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            options.Width = 400;
-            options.Height = 300;
+            options.WindowStartupLocation = WindowStartupLocation.Manual;
+            options.Width = 600;
+            options.Height = 400;
+            options.Margin = new Thickness(10);
+            options.Top = Application.Current.MainWindow.Top + 100;
+            options.Left = Application.Current.MainWindow.Left + 100;
             options.SizeToContent = SizeToContent.Manual;
+
+
 
             windowManager.ShowDialog(viewModel, settings: options);
         }
