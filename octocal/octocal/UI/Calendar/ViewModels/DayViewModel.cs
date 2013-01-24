fff741904a09f84dfefc68005d8cddb5dbf80383
Jan Schulte <janschulte90@googlemail.com>
2013-01-24 18:15:21 +0100

    added timeline to day viewmodel

--- octocal/octocal/UI/Calendar/ViewModels/DayViewModel.cs
+++ octocal/octocal/UI/Calendar/ViewModels/DayViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using Caliburn.Micro;
@@ -27,6 +28,20 @@ namespace octocal.UI.Calendar.ViewModels
             }
         }
 
+        private BindableCollection<HourPart> timeLine;
+
+        public BindableCollection<HourPart> TimeLine
+        {
+            get { return timeLine; }
+            set
+            {
+                if (timeLine == value)
+                    return;
+                timeLine = value;
+                NotifyOfPropertyChange(() => TimeLine);
+            }
+        }
+
         public DayViewModel(IWindsorContainer container,
             IAppointmentService service,
             DateTime currentDayDate)
@@ -34,13 +49,64 @@ namespace octocal.UI.Calendar.ViewModels
             this.service = service;
             this.currentDayDate = currentDayDate;
             DisplayName = "Day Schedule";
+
+            BuildupTimeLine();
+            LoadDaySchedule();
+        }
+
+        private void BuildupTimeLine()
+        {
+            for (var i = 1; i < 25; i++)
+                TimeLine.Add(new HourPart { Hour = i });
         }
 
         private void LoadDaySchedule()
         {
-            Appointments = new BindableCollection<Appointment>
+            var appointments = new BindableCollection<Appointment>
                                     (service.GetAllByStartDate(currentDayDate));
 
+            foreach (var appointment in appointments)
+            {
+                var hourPart = TimeLine.Single(c => c.Hour == appointment.StartDate.Hour);
+
+                hourPart.Appointments.Add(appointment);
+            }
+        }
+    }
+
+    public class HourPart : PropertyChangedBase
+    {
+        private int hour;
+
+        public int Hour
+        {
+            get { return hour; }
+            set
+            {
+                if (hour == value)
+                    return;
+                hour = value;
+                NotifyOfPropertyChange(() => Hour);
+            }
+        }
+
+        private BindableCollection<Appointment> appointments;
+
+        public BindableCollection<Appointment> Appointments
+        {
+            get { return appointments; }
+            set
+            {
+                if (appointments == value)
+                    return;
+                appointments = value;
+                NotifyOfPropertyChange(() => Appointments);
+            }
+        }
+
+        public HourPart()
+        {
+            Appointments = new BindableCollection<Appointment>();
         }
     }
 }
